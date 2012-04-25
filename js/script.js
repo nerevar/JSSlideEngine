19eb9530a1f4ad84e93e5e5fff0eee5d2307b31f
Roman Rybalchenko <mail@nerevar.ru>
2012-04-26 02:51:20 +0400

    converted indents to tabs

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 (function($) {
 	$(function(){
-        /**
-         * Creates new JSSlide object
-         */
+		/**
+		 * Creates new JSSlide object
+		 */
 		JSSlide = new JSSlideClass({
 			totalSlides: 10,
-            container: $('.slide'),
-            slideIdxInput: $('.controls__slideidx')
+			container: $('.slide'),
+			slideIdxInput: $('.controls__slideidx')
 		});
 
-        /**
-         * Check hash in url for slide, and then show specified slide or show first slide
-         */
-        hashResult = /slide(\d)+/.exec(window.location.hash);
-        if (hashResult && hashResult[1]) {
-            JSSlide.show(hashResult[1]);
-        } else {
-            JSSlide.init();
-        }
+		/**
+		 * Check hash in url for slide, and then show specified slide or show first slide
+		 */
+		hashResult = /slide(\d)+/.exec(window.location.hash);
+		if (hashResult && hashResult[1]) {
+			JSSlide.show(hashResult[1]);
+		} else {
+			JSSlide.init();
+		}
 
-        /**
-         * Load page on press Enter
-         */
-        $('.controls__slideidx').keypress(function(e) {
-            if(e.keyCode == 13) {
-                JSSlide.show($(this).val());
-            }
-        });
+		/**
+		 * Load page on press Enter
+		 */
+		$('.controls__slideidx').keypress(function(e) {
+			if(e.keyCode == 13) {
+				JSSlide.show($(this).val());
+			}
+		});
 
-        /**
-         * Url to next slide
-         */
-        $('.controls__next').click(function(e){
-            e.preventDefault();
-            JSSlide.next();
-        });
+		/**
+		 * Url to next slide
+		 */
+		$('.controls__next').click(function(e){
+			e.preventDefault();
+			JSSlide.next();
+		});
 
-        /**
-         * Url to previous slide
-         */
-        $('.controls__prev').click(function(e){
-            e.preventDefault();
-            JSSlide.prev();
-        });
+		/**
+		 * Url to previous slide
+		 */
+		$('.controls__prev').click(function(e){
+			e.preventDefault();
+			JSSlide.prev();
+		});
 
 	});
 })(jQuery);
